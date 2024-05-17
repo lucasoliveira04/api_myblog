aba782c4aa4e3a8dc5eb5be22a0b54d1127b2cb2
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-05-17 11:59:44 -0300

    configuration cross

--- src/main/java/com/my/blog/api_myblog/Controller/ProjectsController.java
+++ src/main/java/com/my/blog/api_myblog/Controller/ProjectsController.java
@@ -1,14 +1,23 @@
 package com.my.blog.api_myblog.Controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.my.blog.api_myblog.DTO.ProjectsDto;
 import com.my.blog.api_myblog.Services.AddProjects.AddProjectServices;
 import com.my.blog.api_myblog.Services.ListAllProjects.ListAllServices;
 import com.my.blog.api_myblog.Services.delete.DeleteServices;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@CrossOrigin(origins= "*", allowedHeaders= "*")
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectsController {
